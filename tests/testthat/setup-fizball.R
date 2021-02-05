db175a6e3756e25839ea227a48cb334e69a9581e
Scott Chamberlain <sckott@fastmail.com>
2021-02-05 12:47:05 -0800

    make vcr setup similar to tidytags - remove failing test

--- tests/testthat/setup-fizball.R
+++ tests/testthat/setup-fizball.R
@@ -1,6 +1,19 @@
 library("vcr") # *Required* as vcr is set up on loading
-invisible(vcr::vcr_configure(
-  dir = vcr::vcr_test_path("fixtures"),
+
+vcr_dir <- vcr::vcr_test_path("fixtures")
+
+if (!nzchar(Sys.getenv('FIZ_KEY'))) {
+  if (dir.exists(vcr_dir)) {
+    Sys.setenv('FIZ_KEY'="11a1111a111a1a1a1a111111a1aa11a1")
+  } else {
+    stop("No API key nor cassettes, tests cannot be run.",
+         call. = FALSE)
+  }
+}
+
+vcr::vcr_configure(
+  turned_off = FALSE,
+  dir = vcr_dir,
   filter_sensitive_data = list("<<<fiz_key>>>" = Sys.getenv('FIZ_KEY'))
-))
+)
 vcr::check_cassette_names()
